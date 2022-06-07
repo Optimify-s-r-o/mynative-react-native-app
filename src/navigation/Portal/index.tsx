3711d83454da435707ec25633082988da89174b4
Marek Frydrych <frydrych.marek@post.cz>
2022-06-07 11:17:35 +0200

    imports fix

--- src/navigation/Portal/index.tsx
+++ src/navigation/Portal/index.tsx
@@ -1,48 +1,42 @@
-import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
-import {RootTabParamList, RootTabScreenProps} from "../../navigation/types";
-import useColorScheme from "../../hooks/useColorScheme";
-import {colours} from "../../theme";
-import TabOneScreen from "../../screens/TabOneScreen";
-import {FontAwesome} from "@expo/vector-icons";
-import TabTwoScreen from "../../screens/TabTwoScreen";
-import * as React from "react";
-import { Routes } from "../../constants/routes";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { RootTabParamList, RootTabScreenProps } from '../../navigation/types';
+import useColorScheme from '../../hooks/useColorScheme';
+import TabOneScreen from '../../screens/TabOneScreen';
+import { FontAwesome } from '@expo/vector-icons';
+import TabTwoScreen from '../../screens/TabTwoScreen';
+import * as React from 'react';
+import { Routes } from '../../constants/routes';
 
 const BottomTab = createBottomTabNavigator<RootTabParamList>();
 
 export const PortalTabNavigator = () => {
-    const colorScheme = useColorScheme();
+  const colorScheme = useColorScheme();
 
-    return (
-        <BottomTab.Navigator
-            initialRouteName={Routes.TabOne}
-            screenOptions={{
-                tabBarActiveTintColor: colours[colorScheme].tint,
-            }}
-        >
-            <BottomTab.Screen
-                name={Routes.TabOne}
-                component={TabOneScreen}
-                options={({ navigation }: RootTabScreenProps<'TabOne'>) => ({
-                    title: 'Tab One',
-                    tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-                })}
-            />
-            <BottomTab.Screen
-                name={Routes.TabTwo}
-                component={TabTwoScreen}
-                options={{
-                    title: 'Tab Two',
-                    tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-                }}
-            />
-        </BottomTab.Navigator>
-    );
-}
+  return (
+    <BottomTab.Navigator initialRouteName={Routes.TabOne}>
+      <BottomTab.Screen
+        name={Routes.TabOne}
+        component={TabOneScreen}
+        options={({ navigation }: RootTabScreenProps<'TabOne'>) => ({
+          title: 'Tab One',
+          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+        })}
+      />
+      <BottomTab.Screen
+        name={Routes.TabTwo}
+        component={TabTwoScreen}
+        options={{
+          title: 'Tab Two',
+          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+        }}
+      />
+    </BottomTab.Navigator>
+  );
+};
 
 function TabBarIcon(props: {
-    name: React.ComponentProps<typeof FontAwesome>['name'];
-    color: string;
+  name: React.ComponentProps<typeof FontAwesome>['name'];
+  color: string;
 }) {
-    return <FontAwesome size={30} style={{ marginBottom: -3 }} {...props} />;
+  return <FontAwesome size={30} style={{ marginBottom: -3 }} {...props} />;
 }
